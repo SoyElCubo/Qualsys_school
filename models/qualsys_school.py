2fed37e2f07fd5cf86cefe24af2c7e571afa9896
cesarb_qualsys <cesar_bonilla@qualsys.com.mx>
2020-10-09 03:04:58 -0500

    Campos añadidos             ▪ name, tipo char requerido.             ▪ street, tipo char.             ▪ street_number, tipo char.             ▪ city, tipo char requerido.             ▪ state, tipo M2o (res.country.state), requerido.             ▪ country, tipo M2o (res.country), requerido.             ▪ phone_one, tipo char requerido.             ▪ phone_two, tipo char.             ▪ email, tipo char.             ▪ courses_number, tipo integer, computado.             ▪ courses_ids tipo O2m (qualsys.courses)

--- models/qualsys_school.py
+++ models/qualsys_school.py
@@ -8,10 +8,10 @@ class QualsysSchool(models.Model):
     _name = 'qualsys.school'
     _description = 'Creando el primer modulo'
 
-    # @api.depend('courses_id')
-    # def get_courses_number(self):
-    #     for data in self:
-    #         data.courses_number = len(data.courses_ids)
+    @api.depends('courses_id')
+    def get_courses_number(self):
+         for data in self:
+             data.courses_number = len(data.courses_id)
 
     name = fields.Char(string = "Nombre", required=True)
     street = fields.Char(string="Calle")
@@ -19,6 +19,10 @@ class QualsysSchool(models.Model):
     city = fields.Char(string="Ciudad", required=True)
     state = fields.Many2one('res.country', string="Estado", required=True)
     country = fields.Many2one('res.country', string="Pais", required=True)
+    phone_one = fields.Char(string="Telefono Uno", required=True)
+    phone_two = fields.Char(string="Telefono Dos", required=True)
+    email = fields.Char(string="Correo electronico")
+    courses_number = fields.Integer(compute=get_courses_number, string="No. de Cursos")
     courses_id = fields.One2many('qualsys.courses', 'school_id', string=" Cursos")
 
 
