b2471b6b56f1109ca61b8742454e6ccdc219c380
cesarb_qualsys <cesar_bonilla@qualsys.com.mx>
2020-10-12 15:05:32 -0500

    Se empieza la creacion del wizard
    - Se logra ver el pop-up de cursos disponibles

--- wizard/courseAssignWizard.py
+++ wizard/courseAssignWizard.py
@@ -4,6 +4,8 @@ _logger = logging.getLogger(__name__)
 from odoo import models, api, fields
 from odoo.exceptions import UserError
 
-class CourseAssignWizard(model.TrasientModel):
+class CourseAssignWizard(models.TransientModel):
     _name = "course.assign.wizard"
     _description = "Asignacion de cursos a usuario existente"
+
+    courses_id = fields.Many2many('qualsys.courses', string="Cursos")
